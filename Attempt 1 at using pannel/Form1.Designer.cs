f04faf2dc14582750de9822d88921080423b6693
Seth Wagener <sethwagener@mmc.school.nz>
2021-06-21 09:15:22 +1200

    Add project files.

---
+++ Attempt 1 at using pannel/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace Attempt_1_at_using_pannel
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Game_Pnl = new System.Windows.Forms.Panel();
+            this.Fuel_Lbl = new System.Windows.Forms.Label();
+            this.Return_Btn = new System.Windows.Forms.Button();
+            this.Save_Btn = new System.Windows.Forms.Button();
+            this.Load_Btn = new System.Windows.Forms.Button();
+            this.Framerate = new System.Windows.Forms.Timer(this.components);
+            this.Torch_Tmr = new System.Windows.Forms.Timer(this.components);
+            this.Game_Pnl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Game_Pnl
+            // 
+            this.Game_Pnl.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Game_Pnl.Controls.Add(this.Fuel_Lbl);
+            this.Game_Pnl.Controls.Add(this.Return_Btn);
+            this.Game_Pnl.Controls.Add(this.Save_Btn);
+            this.Game_Pnl.Controls.Add(this.Load_Btn);
+            this.Game_Pnl.Location = new System.Drawing.Point(1, 0);
+            this.Game_Pnl.Name = "Game_Pnl";
+            this.Game_Pnl.Size = new System.Drawing.Size(797, 451);
+            this.Game_Pnl.TabIndex = 0;
+            this.Game_Pnl.TabStop = true;
+            this.Game_Pnl.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Fuel_Lbl
+            // 
+            this.Fuel_Lbl.AutoSize = true;
+            this.Fuel_Lbl.BackColor = System.Drawing.Color.Black;
+            this.Fuel_Lbl.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Fuel_Lbl.ForeColor = System.Drawing.Color.OrangeRed;
+            this.Fuel_Lbl.Location = new System.Drawing.Point(681, 7);
+            this.Fuel_Lbl.Name = "Fuel_Lbl";
+            this.Fuel_Lbl.Size = new System.Drawing.Size(95, 30);
+            this.Fuel_Lbl.TabIndex = 4;
+            this.Fuel_Lbl.Text = "Fuel Left";
+            // 
+            // Return_Btn
+            // 
+            this.Return_Btn.AutoSize = true;
+            this.Return_Btn.Location = new System.Drawing.Point(162, 0);
+            this.Return_Btn.Name = "Return_Btn";
+            this.Return_Btn.Size = new System.Drawing.Size(96, 25);
+            this.Return_Btn.TabIndex = 3;
+            this.Return_Btn.Text = "Back to game";
+            this.Return_Btn.UseVisualStyleBackColor = true;
+            this.Return_Btn.Click += new System.EventHandler(this.Return_Btn_Click);
+            // 
+            // Save_Btn
+            // 
+            this.Save_Btn.Location = new System.Drawing.Point(81, 0);
+            this.Save_Btn.Name = "Save_Btn";
+            this.Save_Btn.Size = new System.Drawing.Size(75, 23);
+            this.Save_Btn.TabIndex = 2;
+            this.Save_Btn.TabStop = false;
+            this.Save_Btn.Text = "Save";
+            this.Save_Btn.UseVisualStyleBackColor = true;
+            this.Save_Btn.Click += new System.EventHandler(this.Save_Btn_Click);
+            // 
+            // Load_Btn
+            // 
+            this.Load_Btn.Location = new System.Drawing.Point(0, 0);
+            this.Load_Btn.Name = "Load_Btn";
+            this.Load_Btn.Size = new System.Drawing.Size(75, 23);
+            this.Load_Btn.TabIndex = 1;
+            this.Load_Btn.TabStop = false;
+            this.Load_Btn.Text = "Load";
+            this.Load_Btn.UseVisualStyleBackColor = true;
+            this.Load_Btn.Click += new System.EventHandler(this.Load_Btn_Click);
+            // 
+            // Framerate
+            // 
+            this.Framerate.Enabled = true;
+            this.Framerate.Interval = 20;
+            this.Framerate.Tick += new System.EventHandler(this.Framerate_Tick);
+            // 
+            // Torch_Tmr
+            // 
+            this.Torch_Tmr.Interval = 1000;
+            this.Torch_Tmr.Tick += new System.EventHandler(this.Torch_Tmr_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Game_Pnl);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.Game_Pnl.ResumeLayout(false);
+            this.Game_Pnl.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Game_Pnl;
+        private System.Windows.Forms.Button Load_Btn;
+        private System.Windows.Forms.Button Save_Btn;
+        private System.Windows.Forms.Timer Framerate;
+        private System.Windows.Forms.Button Return_Btn;
+        private System.Windows.Forms.Label Fuel_Lbl;
+        private System.Windows.Forms.Timer Torch_Tmr;
+    }
+}
+
---
+++ Attempt 1 at using pannel/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+using System.Drawing.Drawing2D;
+
+namespace Attempt_1_at_using_pannel
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        Rectangle[] UpS = new Rectangle[7];
+        Rectangle[] DownS = new Rectangle[7];
+        Rectangle[] LeftS = new Rectangle[7];
+        Rectangle[] RightS = new Rectangle[7];
+        Rectangle[] Object = new Rectangle[7];
+        Rectangle Player, LSide, TSide, BSide, RSide, LightBar;
+        string line;
+        int Py, Px, LightArea = 250, LightBarLngth, Xmovement, Ymovement, Xshift;
+        double Fuel = 1.0;
+        bool left, right, up, jump;
+        int[,] Var = new int[3,2]
+        {
+            {0,0},
+            {1,0},
+            {2,0}
+        };
+        Image player = Image.FromFile(Application.StartupPath + @"\Player.png");
+        public Form1()
+        {
+            InitializeComponent();
+            typeof(Panel).InvokeMember("DoubleBuffered", BindingFlags.SetProperty | BindingFlags.Instance | BindingFlags.NonPublic, null, Game_Pnl, new object[] { true });
+            Framerate.Enabled = false;
+            Ymovement = -5;
+            Xmovement = 0;
+            Py = 10;
+            Object[1] = new Rectangle(0, 300, 100, 50); //this is ground
+            Object[2] = new Rectangle(0, 400, 5000, 50); // this is the ground
+            Object[3] = new Rectangle(500, 300, 100, 50);
+            Object[4] = new Rectangle(300, 250, 150, 30);
+            for (int O = 1; O < 7; O++)
+            {
+                UpS[O] = new Rectangle(Object[O].Left, Object[O].Top, Object[O].Width, 10);
+                RightS[O] = new Rectangle(Object[O].Right-5, Object[O].Top + 5, 5, Object[O].Height - 5);
+                LeftS[O] = new Rectangle(Object[O].Left, Object[O].Top + 5, 5, Object[O].Height - 5);
+                DownS[O] = new Rectangle(Object[O].Left, Object[O].Bottom-5, Object[O].Width, 10);
+            }
+        }
+
+        private void Save_Btn_Click(object sender, EventArgs e)
+        {
+            Var[0, 1] = Px;
+            Var[1, 1] = Py;
+            TextWriter Save = new StreamWriter(Application.StartupPath + @"\Test.txt");
+
+            //Writing text to the file.
+            Save.WriteLine("Hello this line is skipped by the loading system so I just added this here and wanted to say hi!");
+            Save.WriteLine(Var[0, 1]);
+            Save.WriteLine(Var[1, 1]);
+            System.Windows.Forms.MessageBox.Show("File Saved");
+
+            //Close the file.
+            Save.Close();
+        }
+        private void Load_Btn_Click(object sender, EventArgs e)
+        {
+            int ID = 0;
+
+            System.IO.StreamReader file = new System.IO.StreamReader(Application.StartupPath + @"\Test.txt");
+            file.ReadLine();
+            while ((line = file.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+                Var[ID, 1] = Convert.ToInt32(line);
+                ID++;
+            }
+            Px = Var[0, 1];
+            Py = Var[1, 1];
+            System.Windows.Forms.MessageBox.Show("File Loaded");
+            file.Close();
+            // Suspend the screen.  
+            // System.Windows.Forms.MessageBox.Show(counter + "");
+
+        }
+        private void Return_Btn_Click(object sender, EventArgs e)
+        {
+            Load_Btn.Enabled = false;
+            Load_Btn.Visible = false;
+            Return_Btn.Enabled = false;
+            Return_Btn.Visible = false;
+            Save_Btn.Enabled = false;
+            Save_Btn.Visible = false;
+            Framerate.Enabled = true;
+            Torch_Tmr.Enabled = true;
+        }
+        private void Framerate_Tick(object sender, EventArgs e)
+        {
+            LightBarLngth = (int)Math.Round(100 * Fuel);
+            Player = new Rectangle(Px, Py, 50, 100);//Player Rectangle
+            LSide = new Rectangle(Px, Py, 5, 100);//Player Rectangle
+            TSide = new Rectangle(Px+5, Py, 40, 5);//Player Rectangle
+            BSide = new Rectangle(Px+5, Py+95, 40, 5);//Player Rectangle
+            RSide = new Rectangle(Px+45, Py, 5, 100);//Player Rectangle
+            LightBar = new Rectangle(685,50 ,LightBarLngth, 20);
+            Game_Pnl.Invalidate();
+            for (int i = 1; i < 7; i++)
+            {
+                if (left == true & !LSide.IntersectsWith(RightS[i]))
+                {
+                    Xmovement = 5;
+                }
+                else if (left == true & LSide.IntersectsWith(RightS[i]))
+                {
+                    Px = RightS[i].Right+4;
+                }
+                else
+                {
+                    Xmovement = 0;
+                }
+            }
+            for (int i = 1; i < 7; i++)
+            {
+                if (right == true & !RSide.IntersectsWith(LeftS[i]))
+                {
+                    Xmovement = -5;
+                }
+                else if (right == true & RSide.IntersectsWith(LeftS[i]))
+                {
+                    Px = LeftS[i].Left- 54;
+                }
+            }
+            for (int i = 1; i < 7; i++)
+            {
+                if (BSide.IntersectsWith(UpS[i]))
+                {
+                    Ymovement = 0;
+                    Py =UpS[i].Top - 100;
+                    jump = true;
+                }
+
+                if (up == true & jump == true)
+                {
+                    Ymovement = 40;
+                    jump = false;
+                }
+                else if (!BSide.IntersectsWith(UpS[i]) & Ymovement >= -15)
+                {
+                        Ymovement = Ymovement - 1;
+                }
+                
+                if(TSide.IntersectsWith(DownS[i]))
+                {
+                    Ymovement = 0;
+                }
+            }
+
+            Px = Px-Xmovement;
+            Py = Py-Ymovement;
+            Game_Pnl.Invalidate();
+        }
+        private void Torch_Tmr_Tick(object sender, EventArgs e)
+        {
+            if(Fuel >= 0)
+            {
+                Fuel -= 0.02;
+                Xshift = Xshift + 2;
+            } 
+            else
+            {
+                Fuel = 0;
+            }
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Left) { left = true; }
+            if (e.KeyData == Keys.Right) { right = true; }
+            if (e.KeyData == Keys.Up) { up = true; }
+        }
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Left) { left = false; }
+            if (e.KeyData == Keys.Right) { right = false; }
+            if (e.KeyData == Keys.Up) { up = false; }
+            if (e.KeyData == Keys.Escape)
+            {
+                Load_Btn.Enabled = true;
+                Load_Btn.Visible = true;
+                Return_Btn.Enabled = true;
+                Return_Btn.Visible = true;
+                Save_Btn.Enabled = true;
+                Save_Btn.Visible = true;
+                Framerate.Enabled = false;
+                Torch_Tmr.Enabled = false;
+            }
+        }
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            g = e.Graphics;
+            //use the DrawImage method to draw the spaceship on the panel
+            g.DrawImage(player, Player);
+            //use the DrawImage method to draw the planet on the panel
+            e.Graphics.FillRectangle(Brushes.Green, TSide);
+            e.Graphics.FillRectangle(Brushes.Green, BSide);
+            e.Graphics.FillRectangle(Brushes.Green, RSide);
+            e.Graphics.FillRectangle(Brushes.Green, LSide);
+            e.Graphics.FillRectangle(Brushes.Black, Object[1]);
+            e.Graphics.FillRectangle(Brushes.Black, Object[2]);
+            e.Graphics.FillRectangle(Brushes.Black, Object[3]);
+            e.Graphics.FillRectangle(Brushes.Black, Object[4]);
+            var rgn = new Region(new Rectangle(0, 0, 1000, 1000));
+            var path = new GraphicsPath();
+           if(Fuel <= 1 & Fuel >=0.6)
+           { 
+                path.AddEllipse(Px - 120 + Xshift, Py - 75, (int)(LightArea * (Fuel + 0.2)), (int)(LightArea * (Fuel + 0.2)));
+            }
+           else if (Fuel <= 0.6 & Fuel >= 0.25)
+            {
+                path.AddEllipse(Px - 120 + Xshift, Py - 50, (int)(LightArea * (Fuel + 0.2)), (int)(LightArea * (Fuel + 0.2)));
+
+            }
+            else if (Fuel <= 0.25 & Fuel >= 0.01)
+            {
+                path.AddEllipse(Px - 110 + Xshift, Py - 30, (int)(LightArea * (Fuel + 0.2)), (int)(LightArea * (Fuel + 0.2)));
+            }
+                rgn.Exclude(path);
+            e.Graphics.FillRegion(Brushes.Black, rgn);
+            e.Graphics.FillRectangle(Brushes.OrangeRed, LightBar);
+        }
+    }
+}
